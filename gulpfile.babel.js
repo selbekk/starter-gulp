203dea00d0d69357200d524568274bd61e76d47d
selbekk <kristofer.selbekk@bekk.no>
2016-02-25 11:06:58 -0800

    Set up a bootstrapped simple gulpfile with ES2015 support

---
+++ gulpfile.babel.js
@@ -0,0 +1,16 @@
+import gulp from 'gulp';
+
+/*
+    You can now implement the tasks you want implemented. Notice that you
+    have full ES2015 support, thanks to Babel :)
+ */
+
+gulp.task('scripts', () => {
+    console.error('scripts task not implemented yet');
+});
+
+gulp.task('styles', () => {
+    console.error('styles task not implemented yet');
+});
+
+gulp.task('default', ['scripts', 'styles']);
